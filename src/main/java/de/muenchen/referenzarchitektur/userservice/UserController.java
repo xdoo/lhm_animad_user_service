59bd8029969953fd46de9766d59e68311b03b8e9
rowe42 <rolandwrn@gmail.com>
2018-01-30 16:55:42 +0100

    Erster Versuch Permissions als HAL einzubauen

--- src/main/java/de/muenchen/referenzarchitektur/userservice/UserController.java
+++ src/main/java/de/muenchen/referenzarchitektur/userservice/UserController.java
@@ -1,12 +1,19 @@
 package de.muenchen.referenzarchitektur.userservice;
 
 import de.muenchen.referenzarchitektur.authorisationLib.EntitlementsService;
+import de.muenchen.referenzarchitektur.userservice.domain.PermissionsResource;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.logging.Logger;
+import org.springframework.hateoas.Link;
+import static org.springframework.hateoas.jaxrs.JaxRsLinkBuilder.linkTo;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
@@ -18,39 +25,51 @@ public class UserController {
 
     private final EntitlementsService entitlementsService;
 
-
     private static final Logger LOG = Logger.getLogger(UserController.class.getName());
 
     public UserController(EntitlementsService entitlementsService) {
         this.entitlementsService = entitlementsService;
     }
 
-
     @RequestMapping(value = "/getPermissions", method = RequestMethod.GET)
     public String getPermissions() {
-        Set<String> permissions = entitlementsService.getPermissions(false);
+        Set<String> permissions = entitlementsService.getPermissions(true, false);
         if (permissions != null) {
             return permissions.toString();
         } else {
             return "No Permissions found.";
         }
     }
+    
+    @RequestMapping(value = "/getPermissions", method = RequestMethod.OPTIONS)
+    public String getOptions() {
+        return "Body";
+    }    
 
+    @CrossOrigin(origins = "http://127.0.0.1:8081")
     @RequestMapping(value = "/getPermissionsDummy", method = RequestMethod.GET)
-    public Set<String> getPermissionsDummy() {
+    public ResponseEntity<PermissionsResource> getPermissionsDummy() {
         LOG.info("Called getPermissionsDummy");
-        Set<String> permissions = new HashSet<String>();
+        Set<String> permissions = new HashSet<>();
         permissions.add("RESOURCE3");
         permissions.add("RESOURCE2");
         permissions.add("RESOURCE1");
         permissions.add("Default Resource");
+
+        PermissionsResource permissionsResource = new PermissionsResource();
+        permissionsResource.setPermissions(permissions);
+
+        Link link = linkTo(UserController.class).withSelfRel();
+        permissionsResource.add(link);
+        
         try {
             Thread.sleep(3000);
         } catch (InterruptedException ex) {
             Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
         }
         LOG.info("Returning getPermissionsDummy");
-        return permissions;
+//        return permissionsResource;
+        return new ResponseEntity<PermissionsResource>(permissionsResource, HttpStatus.FOUND);
     }
 
 }
